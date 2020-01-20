0a5d18183ba610c8cee3e11225ab88c74a06b44f
Sho Asato <e195750@ie.u-ryukyu.ac.jp>
2020-01-20 23:43:37 +0900

    レポート６−２

--- src/main/java/jp/ac/uryukyu/ie/e195750/Game.java
+++ src/main/java/jp/ac/uryukyu/ie/e195750/Game.java
@@ -3,30 +3,53 @@ package jp.ac.uryukyu.ie.e195750;
 
 import java.util.Scanner;
 
-public class Game {
+public  class Game {
     String question;
 
 
+
     public Game(String question) {
+
         this.question = question;
     }
 
     public String toString() {
+
         return question;
     }
 
-    Scanner sc = new Scanner(System.in);
-    int sntence = sc.nextInt();
 
 
 
-}
-
-class Contorol{
-    public void start(){
-        System.out.println("これからクイズをはじめます。");
     }
+class Game1{
+
 }
 
 
+
+    class Contorol {
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
+
+        void start(){
+            System.out.println("これか");
+        }
+
+
+        public void answer() {
+            String ans = null;
+
+            if (this.d == 0) {
+                ans = "seikai";
+                System.out.println(ans);
+            }
+
+
+
+
+
+        }
+
+    }
 
--- src/main/java/jp/ac/uryukyu/ie/e195750/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e195750/Main.java
@@ -4,23 +4,39 @@ package jp.ac.uryukyu.ie.e195750;
 public class Main {
     public static void main(String[] args){
 
-        Contorol contorol = new Contorol();
-        contorol.start();
+
 
         Game[] game = {
-            new Game("(1)　現在の元号は？"),
-            new Game(""),
+            new Game("現在の元号は？"),
+            new Game("1 平成"),
+            new Game("2 令和"),
+            new Game("3 昭和"),
 
         };
+        System.out.println(game[0]);
+        System.out.println(game[1]);
+        System.out.println(game[2]);
+        System.out.println(game[3]);
 
-        for(int i=0; i<game.length; i++){
+
+        Game1 game1s = new Game1();
+
+        Contorol contorol = new Contorol();
+
+
+
+
+
+
+        /*for(int i=0; i<game.length; i++){
             System.out.println(game[i].toString());
-        }
-
-
-
+        }**/
+        contorol.start();
+        contorol.answer();
 
 
 
     }
+
+
 }
