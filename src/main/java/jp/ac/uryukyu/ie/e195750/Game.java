bdad26903dec378fe2242c1ba8c55028845aee77
Sho Asato <e195750@ie.u-ryukyu.ac.jp>
2020-01-21 04:03:20 +0900

    レポート６−３

--- src/main/java/jp/ac/uryukyu/ie/e195750/Game.java
+++ src/main/java/jp/ac/uryukyu/ie/e195750/Game.java
@@ -1,15 +1,26 @@
 package jp.ac.uryukyu.ie.e195750;
 
-
 import java.util.Scanner;
 
+/**
+ * Gameクラス
+ */
 public  class Game {
     String question;
 
+    /**
+     *コンストラクタ
+     * @param question　問題文
+     */
     public Game(String question) {
 
         this.question = question;
     }
+
+    /**
+     *getterメソッドと同様
+     * @return
+     */
 
     public String toString() {
 
@@ -19,11 +30,25 @@ public  class Game {
 
 }
 
+/**
+ * 入力された数字を取り込む
+ */
 
-class Quiz1 {
+class Scan{
     Scanner sc = new Scanner(System.in);
     int d = sc.nextInt();
 
+}
+
+/**
+ *Scanを継承
+ * Quiz1クラス
+ */
+class Quiz1 extends Scan{
+
+    /**
+     *正解か不正解かを判定するメソッド
+     */
 
     public void answer() {
         String ans = null;
@@ -46,9 +71,7 @@ class Quiz1 {
 
     }
 
-class Quiz2 {
-    Scanner sc = new Scanner(System.in);
-    int d = sc.nextInt();
+class Quiz2 extends Scan  {
 
 
     public void answer() {
@@ -64,6 +87,53 @@ class Quiz2 {
         }
         else if(this.d == 3){
             ans = "正解！";
+            System.out.println(ans);
+        }
+
+
+    }
+
+}
+
+class Quiz3 extends Scan{
+
+
+    public void answer() {
+        String ans = null;
+
+        if (this.d == 1) {
+            ans = "正解は3です";
+            System.out.println(ans);
+        }
+        else if(this.d == 2){
+            ans = "正解は3です";
+            System.out.println(ans);
+        }
+        else if(this.d == 3){
+            ans = "正解！";
+            System.out.println(ans);
+        }
+
+
+    }
+
+}
+
+class Quiz4 extends Scan{
+
+    public void answer() {
+        String ans = null;
+
+        if (this.d == 1) {
+            ans = "正解は2です";
+            System.out.println(ans);
+        }
+        else if(this.d == 2){
+            ans = "正解!";
+            System.out.println(ans);
+        }
+        else if(this.d == 3){
+            ans = "正解は2です";
             System.out.println(ans);
         }
 
--- src/main/java/jp/ac/uryukyu/ie/e195750/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e195750/Main.java
@@ -3,9 +3,6 @@ package jp.ac.uryukyu.ie.e195750;
 
 public class Main {
     public static void main(String[] args){
-
-
-
         Game[] game = {
             new Game("オーストラリアの首都はどこでしょうか？"),
             new Game("1 キャンベラ"),
@@ -15,15 +12,18 @@ public class Main {
             new Game("1 20時間"),
             new Game("2 8時間"),
             new Game("3 20分"),
-
+            new Game("古代ローマの人が洗剤がわりに使っていたものは？"),
+            new Game("1 ワイン"),
+            new Game("2 砂"),
+            new Game("3 おしっこ"),
+            new Game("トイレでよく見かけるトイレットペーパーの三角折の意味はなんでしょう？"),
+            new Game("1 幸運のおまじない"),
+            new Game("2 「清掃完了しました」という合図"),
+            new Game("3 紙を引き出しやすいように"),
         };
-
         for(int i=0; i<4;i++) {
             System.out.println(game[i]);
         }
-
-
-
         Quiz1 quiz1 = new Quiz1();
         quiz1.answer();
 
@@ -33,9 +33,16 @@ public class Main {
         Quiz2 quiz2 = new Quiz2();
         quiz2.answer();
 
+        for(int i=8; i<12; i++){
+            System.out.println(game[i]);
+        }
+        Quiz3 quiz3 = new Quiz3();
+        quiz3.answer();
 
-
+        for (int i=12; i<16; i++){
+            System.out.println(game[i]);
+        }
+        Quiz4 quiz4 = new Quiz4();
+        quiz4.answer();
     }
-
-
 }
